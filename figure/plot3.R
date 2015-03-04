487f9362e3957ad3e5142bb537a62542c51c9f38
chen-hongbo <18906912722@189.cn>
2015-03-04 16:32:20 +0800

    change in 4 mar

--- figure/plot3.R
+++ figure/plot3.R
@@ -4,16 +4,15 @@ plot3 <- function() {
     ansy_data <- subset(data_set,as.character(data_set$Date) %in% c("1/2/2007","2/2/2007"))
     ansy_data$Sub_metering_1 <- as.numeric(as.character(ansy_data[,"Sub_metering_1"]))
     ansy_data$Sub_metering_2 <- as.numeric(as.character(ansy_data[,"Sub_metering_2"]))
-    par(fin = c(4,4))
     par(cex.axis = 1 )
     with( ansy_data, {
-       plot.ts(Sub_metering_1,col="gray",xaxt="n",ylim=c(0,40),xlim=c(0,2880),ylab="Energy Sub Metering",xlab="")
+       plot.ts(Sub_metering_1,col="black",xaxt="n",ylim=c(0,40),xlim=c(0,2880),ylab="Energy Sub Metering",xlab="")
        par(new = TRUE)
        plot.ts(Sub_metering_2,col="red",xaxt="n",ylim=c(0,40),xlim=c(0,2880),ylab="",xlab="")
        par(new = TRUE)
        plot.ts(Sub_metering_3,col="blue",xaxt="n",ylim=c(0,40),xlim=c(0,2880),ylab="",xlab="")
        axis(1,at= c(0,1440,2880),labels=c("Thu","Fri","Sat"))
-       legend("topright", lty = 1, cex = 0.5, col = c("gray","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+       legend("topright", lty = 1, cex = 0.5, col = c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
        dev.copy(png,file = "plot3.png", heigh = 480,width = 480)
        dev.off()
     }
